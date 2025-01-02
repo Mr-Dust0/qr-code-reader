faac8a8181c58d24e6ac5e4a84ddfaedda1ebb7b
Luke <luke61996@gmail.com>
2025-01-02 13:15:15 +0000

    Added comments to logger.py

--- logger.py
+++ logger.py
@@ -3,39 +3,34 @@ import platform
 import os
 import json
 import requests
+from config import URL
 
-URL = "https://192.168.5.102"
 
 def logPdfOpen(file: str):
-    current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
     hostname = platform.node()
     data = {
-        "TimeStamp": current_time,
         "HostName": hostname,
         "FileName":file 
     }
     headers = {"Content-Type": "application/json"}
     try:
+        # Send json to the log server to log what file has been opened and by what hostname
+        # Dont send the time because the time can be off on the pi
         response = requests.post(URL + "/log", data=json.dumps(data), headers=headers, verify=False)
-        print(response.status_code)
         print(response.text)
     except Exception as e:
-        print(URL)
-        print(e)
         print("Cant reach centernal server")
         os.system('dunstify "Cant reach centeral server"')
 
 def logPdfClose():
     hostname = platform.node()
-    current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
     data = {
         "HostName": hostname,
-        "TimeStampClosed":current_time 
     }
     headers = {"Content-Type": "application/json"}
     try:
+        # Send hostname to centeral log server and that will mark the most recent opened file by the hostname as closed.
         response = requests.put(URL + "/fileclosed", data=json.dumps(data), headers=headers, verify=False)
-        print(response.status_code)
         print(response.text)
     except Exception as e:
         print(URL)
